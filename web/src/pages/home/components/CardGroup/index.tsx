f772b5b21410430761a9af50cc56f8d22bec3dc6
gl <452392920@qq.com>
2019-12-22 20:59:01 +0800

    update：卡牌组件

--- web/src/pages/home/components/CardGroup/index.tsx
+++ web/src/pages/home/components/CardGroup/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useCallback, useRef, useMemo, createRef, useState } from 'react'
 import styles from './index.scss'
 import cs from 'classnames'
+import Ctx from './context'
 interface CardGroupProps extends React.Props<{}> {
   top?: number
   bottom?: number
@@ -11,6 +12,7 @@ function CardGroup(props: CardGroupProps) {
   const [init, setInit] = useState(false)
   const groupRef = useRef<HTMLDivElement>()
   const { top = 0, right = 0, bottom = 0, left = 0 } = props
+  const trigger = useShake()
 
   const resizeChildren = useCallback(() => {
     const containerRect = (groupRef.current as HTMLDivElement).getBoundingClientRect()
@@ -27,7 +29,7 @@ function CardGroup(props: CardGroupProps) {
     for (let i = 0, len = collection.length; i < len; i++) {
       const child = collection[i] as HTMLDivElement
       const childRect = child.getBoundingClientRect()
-      const {index, value} = getMinItemIndexInArray(TopArr)
+      const { index, value } = getMinItemIndexInArray(TopArr)
       child.style.setProperty('top', `${value}px`)
       child.style.setProperty('left', `${index * offset}px`)
       child.style.setProperty('position', 'absolute')
@@ -38,8 +40,8 @@ function CardGroup(props: CardGroupProps) {
   }, [top, right, bottom, left])
 
   const handleWindowResize = useCallback(() => {
-    resizeChildren()
-  }, [])
+    trigger(resizeChildren, 200)
+  }, [trigger])
 
   useEffect(() => {
     window.addEventListener('resize', handleWindowResize)
@@ -51,9 +53,11 @@ function CardGroup(props: CardGroupProps) {
   }, [])
 
   return (
-    <div className={cs(styles.cardGroup, {[styles.show]: init})} ref={groupRef as any}>
-      {props.children}
-    </div>
+    <Ctx.Provider value={[]}>
+      <div className={cs(styles.cardGroup, { [styles.show]: init })} ref={groupRef as any}>
+        {props.children}
+      </div>
+    </Ctx.Provider>
   )
 }
 
@@ -74,19 +78,33 @@ function createArray<T>(len: number, initValue: T): Array<T> {
  * 获取数组中最小的数
  * @param arr 
  */
-function getMinItemIndexInArray(arr: number[]): {index: number, value: number} {
+function getMinItemIndexInArray(arr: number[]): { index: number, value: number } {
   return arr.reduce((previous, value, index) => {
     if (value < previous.value) {
       previous.value = value
       previous.index = index
     }
     return previous
-  }, {index: -1, value: Number.MAX_SAFE_INTEGER})
+  }, { index: -1, value: Number.MAX_SAFE_INTEGER })
 }
 
 // 防抖钩子
 function useShake() {
+  const ref = useRef<Function>()
+  const timer = useMemo<{ id: number }>(() => ({ id: -1 }), [])
 
+  const trigger = useCallback((callFunc: Function, duration: number) => {
+    ref.current = callFunc
+    clearTimeout(timer.id)
+    timer.id = setTimeout(() => {
+      ref.current && ref.current()
+    }, duration) as any;
+  }, [])
+
+  useEffect(() => () => {
+    clearTimeout(timer.id)
+  }, [])
+  return trigger
 }
 
 
