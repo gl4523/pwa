782d4134c68080c80f614dbfadb10d7ae3df1caa
gl <452392920@qq.com>
2020-03-08 21:50:43 +0800

    a

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,10 +7,6 @@ ReactDOM.render(<div>
   <h2>hello pwa</h2>
 </div>, document.querySelector('#root'))
 
-if (module) {
-  (module as any).hot.accept()
-}
-
 if (typeof navigator.serviceWorker !== 'undefined') {
     navigator.serviceWorker.register('https://gl4523.github.io/pwa/js/sw.js').then(registration => {
       console.log(registration )
